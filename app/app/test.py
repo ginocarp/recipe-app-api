d32e8c9c805081d175fdbf0e65e787f5b401310e
= <=>
2023-10-27 14:19:04 +0200

    Configuración BD parte 1

---
+++ app/app/test.py
@@ -0,0 +1,17 @@
+"""Sample test"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+
+    def test_subract_numbers(self):
+        res = calc.subtract(10, 15)
+        self.assertEquals(res, 5)
+
+
+
